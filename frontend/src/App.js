06ae947b705597a7c9bf52d853d8ceb978c6720b
jimdac3464 <jimdac3464@gmail.com>
2022-10-27 14:20:06 +0900

    [FE-yjh-0-1] feat: Nav 와 footer 완성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,22 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import KakaoLogin from "./components/Login/KakaoLogin.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import "../src/styles/Common/App.scss";
+import TopNav from "./components/Common/TopNav.jsx";
+import Footer from "./components/Common/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/oauth/callback/kakao" element={<KakaoLogin />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path="" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
