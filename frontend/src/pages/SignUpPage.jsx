94acf43945d5015f2d60ea99ba8f5a1502ebe81d
임현탁 <seoktak123@gmail.com>
2022-10-27 10:47:10 +0900

    [FE-HT-0-1] feat: 회원가입 모든 박스 구현

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -6,11 +6,34 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
-
+import { Button } from '@mui/material';
+import axios from 'axios'
 const SignUpPage = () => {
     const [region, setRegion] = useState('')
+    const [name, setName] = useState('')
+    const [nickname, setNickname] = useState('')
     const handleRegion = (event) => {
         setRegion(event.target.value)
+    }
+    const handleName = (event) => {
+        setName(event.target.value)
+    }
+    const handleNickname = (event) => {
+        setNickname(event.target.value)
+    }
+    const handleSignup = () => {
+        const userInfo={
+            'region' : region,
+            'name' : name,
+            'nickname' : nickname
+        }
+        console.log(userInfo)
+        // axios.post('url', userInfo)
+    }
+    const handleNicknameCheck=()=>{
+        const Nicknameinfo={
+            'nickname' : nickname
+        }
     }
     return(
         <div>
@@ -33,9 +56,15 @@ const SignUpPage = () => {
                         <MenuItem value={'부울경'}>부울경</MenuItem>
                     </Select>
                 </FormControl>
-                <TextField>
-                    
-                </TextField>
+                <FormControl fullWidth>
+                    <TextField label="이름" variant="outlined" value={name} onChange={handleName}/>
+                </FormControl>
+                <FormControl fullWidth>
+                    <TextField label="닉네임" variant="outlined" value={nickname} onChange={handleNickname}/>
+                </FormControl>
+                <button onClick={handleNicknameCheck}>중복체크</button>
+                <p>최종 활동명은 지역, 이름, 닉네임을 합쳐 지역_이름_닉네임 형식으로 만들어드립니다.</p>
+                <button onClick={handleSignup}>회원가입</button>
             </Box>
         </div>
     );
