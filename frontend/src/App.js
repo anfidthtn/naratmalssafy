22e8d5bcb1d5420ae2354962460035e2faf36649
임현탁 <seoktak123@gmail.com>
2022-10-28 15:56:57 +0900

    [FE-HT-2] feat: 패들릿 기본설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,11 @@ import "../src/styles/Common/App.scss";
 import TopNav from "./components/Common/TopNav.jsx";
 import Footer from "./components/Common/Footer.jsx";
 import PadletPage from "./pages/PadletPage.jsx"
+import Postseoul from "./components/Padlet/Postseoul.jsx"
+import Postbusan from "./components/Padlet/Postbusan.jsx"
+import Postgumi from "./components/Padlet/Postgumi.jsx"
+import Postgwangju from "./components/Padlet/Postgwangju.jsx"
+import Postdaejeon from "./components/Padlet/Postdaejeon.jsx"
 
 
 function App() {
@@ -27,6 +32,11 @@ function App() {
           <Route path="/userinfo" element={<EditUserInfo />} />
           <Route path="/fontinfo/:id" element={<UserFontInfo />} />
           <Route path="/padlet" element={<PadletPage />} />
+          <Route path="/post/seoul" element={<Postseoul />} />
+          <Route path="/post/busan" element={<Postbusan />} />
+          <Route path="/post/daejeon" element={<Postdaejeon />} />
+          <Route path="/post/gwangju" element={<Postgwangju />} />
+          <Route path="/post/gumi" element={<Postgumi />} />
         </Routes>
       </BrowserRouter>
       <Footer />
--- frontend/src/pages/PadletPage.jsx
+++ frontend/src/pages/PadletPage.jsx
@@ -1,9 +1,43 @@
 import '../styles/Padlet/Board.scss'
 import map from '../assets/map.png'
+import seoul from '../assets/seoul.png'
+import daejeon from '../assets/daejeon.png'
+import gwangju from '../assets/gwangju.png'
+import gumi from '../assets/gumi.png'
+import busan from '../assets/busan.png'
+import { useNavigate } from 'react-router-dom'
+
 const PadletPage = () => {
+    const navigate = useNavigate()
+    function navitoSeoul () {
+        navigate('/post/seoul')
+    }
+    function navitoDaejeon () {
+        navigate('/post/daejeon')
+    }
+    function navitoGwangju () {
+        navigate('/post/gwangju')
+    }
+    function navitoGumi () {
+        navigate('/post/gumi')
+    }
+    function navitoBusan () {
+        navigate('/post/busan')
+    }
     return(
         <div className='Padlet'>
-            <img className='Padlet__main' src={map} alt="map"/>
+            <div className='Padlet__West'>
+                <img className='Padlet__West__Seoul' src={seoul} alt="seoul" onClick={navitoSeoul}/>
+                <img className='Padlet__West__Daejeon' src={daejeon} alt="daejeon" onClick={navitoDaejeon}/>
+                <img className='Padlet__West__Gwangju' src={gwangju} alt="gwangju" onClick={navitoBusan}/>
+            </div>
+            <div className='Padlet__main' >
+                <img src={map} alt="map"/>
+            </div>
+            <div className='Padlet__East'>
+                <img className='Padlet__East__Gumi' src={gumi} alt="gumi" onClick={navitoGumi}/>
+                <img className='Padlet__East__Busan' src={busan} alt="busan" onClick={navitoBusan}/>
+            </div>
         </div>
     );
 };
