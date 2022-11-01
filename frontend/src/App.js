fa88454bbcb7e3a9fa1096fd29c08b0daaf460dc
jimdac3464 <jimdac3464@gmail.com>
2022-11-01 17:55:50 +0900

    [FE-yjh-6-1] 폰트 상세보기 페이지 제작 40%

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,18 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import KakaoLogin from "./components/Login/KakaoLogin.jsx";
 import MainPage from "./pages/MainPage.jsx";
 import MyPage from "./pages/MyPage.jsx";
-import EditUserInfo from "./components/Mypage/EditUserInfo.jsx"
+import EditUserInfo from "./components/Mypage/EditUserInfo.jsx";
 import "../src/styles/Common/App.scss";
 import TopNav from "./components/Common/TopNav.jsx";
 import Footer from "./components/Common/Footer.jsx";
-import PadletPage from "./pages/PadletPage.jsx"
-import Postseoul from "./components/Padlet/Postseoul.jsx"
-import Postbusan from "./components/Padlet/Postbusan.jsx"
-import Postgumi from "./components/Padlet/Postgumi.jsx"
-import Postgwangju from "./components/Padlet/Postgwangju.jsx"
-import Postdaejeon from "./components/Padlet/Postdaejeon.jsx"
+import PadletPage from "./pages/PadletPage.jsx";
+import Postseoul from "./components/Padlet/Postseoul.jsx";
+import Postbusan from "./components/Padlet/Postbusan.jsx";
+import Postgumi from "./components/Padlet/Postgumi.jsx";
+import Postgwangju from "./components/Padlet/Postgwangju.jsx";
+import Postdaejeon from "./components/Padlet/Postdaejeon.jsx";
 import FontSearchPage from "./pages/FontSearchPage.jsx";
+import FontDetailPage from "./pages/FontDetailPage.jsx";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path="/oauth/callback/kakao" element={<KakaoLogin />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/" element={<MainPage />} />
+          <Route path="/detail/:id" element={<FontDetailPage />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/userinfo" element={<EditUserInfo />} />
           <Route path="/padlet" element={<PadletPage />} />
---
+++ frontend/src/store/dummy.js
@@ -0,0 +1,62 @@
+export const dummyDataSet = [
+  {
+    id: 1,
+    fontUser: "구미_유지홍_짐닥",
+    fontName: "지홍체",
+    favoriteCount: 140,
+    downloadCount: 300,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap",
+    fontFamilyName: "Do Hyeon",
+  },
+  {
+    id: 2,
+    fontUser: "구미_채민지_2반민지",
+    fontName: "우울할땐 우울체",
+    favoriteCount: 150,
+    downloadCount: 4400,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Poor+Story&display=swap",
+    fontFamilyName: "Poor Story",
+  },
+  {
+    id: 3,
+    fontUser: "구미_한제규_알고리즘왕",
+    fontName: "알고리즘전용체",
+    favoriteCount: 40,
+    downloadCount: 5,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap",
+    fontFamilyName: "Black Han Sans",
+  },
+  {
+    id: 4,
+    fontUser: "구미_가수왕_폰트왕",
+    fontName: "가수체",
+    favoriteCount: 1040,
+    downloadCount: 30000,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap",
+    fontFamilyName: "Nanum Pen Script",
+  },
+  {
+    id: 5,
+    fontUser: "구미_조경수_폰트마마",
+    fontName: "경수우울체",
+    favoriteCount: 350,
+    downloadCount: 100,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Gamja+Flower&display=swap",
+    fontFamilyName: "Gamja Flower",
+  },
+  {
+    id: 6,
+    fontUser: "구미_임현탁_막걸리가좋아",
+    fontName: "현탁막걸리체",
+    favoriteCount: 200,
+    downloadCount: 10,
+    fontDownloadAddress:
+      "https://fonts.googleapis.com/css2?family=Black+And+White+Picture&display=swap",
+    fontFamilyName: "Black And White Picture",
+  },
+];
