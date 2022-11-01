cfe8d7b61e4cef3b22282a50b72b2f0ef9b1e663
가수왕 <kws04254@naver.com>
2022-11-01 16:05:29 +0900

    [BE-SW-2-4] 폰트 목록 반환

--- main/BE/src/main/java/com/ssafy/be/api/dto/TotalResFont.java
+++ main/BE/src/main/java/com/ssafy/be/api/dto/TotalResFont.java
@@ -4,7 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Builder
 @Data
 public class TotalResFont {
+    Long fontSeq;
+    String FontName;
+    String fontPath;
+    Long favCount;
+    String downloadFile;
+    String fileName;
+    String createrName;
+    String createrEmail;
+    String description;
+    Long downloadCount;
+    LocalDateTime regDate;
+    boolean isLike;
 }
--- main/BE/src/main/java/com/ssafy/be/api/service/FontService.java
+++ main/BE/src/main/java/com/ssafy/be/api/service/FontService.java
@@ -2,7 +2,9 @@ package com.ssafy.be.api.service;
 
 import com.ssafy.be.api.response.GetFontsRes;
 import com.ssafy.be.db.entity.User;
+import org.springframework.data.domain.Pageable;
+
 
 public interface FontService {
-    public GetFontsRes getFonts(User user,int page,int CntPerPage);
+    public GetFontsRes getFonts(User user, Pageable pageable);
 }
