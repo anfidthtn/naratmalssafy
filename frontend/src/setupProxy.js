e6a10813c543d69f19cc195b2af4c41dd8d56ea7
임현탁 <seoktak123@gmail.com>
2022-11-04 16:22:22 +0900

    chore: setupproxy.js 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,25 +4,10 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8081",
+      // target: "http://localhost:8081",
+      target: "https://xn--910b35kqzb51p93w.com",
       changeOrigin: true,
-      // pathRewrite: {
-      //   "^/api": "/",
-      // },
     })
   );
 };
-// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "https://xn--910b35kqzb51p93w.com",
-//       changeOrigin: true,
-//       // pathRewrite: {
-//       //   "^/api": "/",
-//       // },
-//     })
-//   );
-// };
