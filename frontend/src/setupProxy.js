a6b90a8608686036118776792c8bdeae1e7aad5e
임현탁 <seoktak123@gmail.com>
2022-11-04 14:44:06 +0900

    Update frontend/src/setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://xn--910b35kqzb51p93w.com",
+      target: "http://xn--910b35kqzb51p93w.com",
       changeOrigin: true,
       // pathRewrite: {
       //   "^/api": "/",
