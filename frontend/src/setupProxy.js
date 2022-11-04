dacddcf76700a8a8ae99c25e162778f6940187d5
임현탁 <seoktak123@gmail.com>
2022-11-04 14:12:31 +0900

    Update frontend/src/pages/LoginPage.jsx, frontend/src/setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8081",
+      target: "https://xn--910b35kqzb51p93w.com:8081",
       changeOrigin: true,
       // pathRewrite: {
       //   "^/api": "/",
