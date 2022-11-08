dc9358f469fcf94949b6dc8cc64db40d324f5043
임현탁 <seoktak123@gmail.com>
2022-11-08 10:02:42 +0900

    Update frontend/src/components/Login/KakaoLogin.jsx

--- frontend/src/components/Login/KakaoLogin.jsx
+++ frontend/src/components/Login/KakaoLogin.jsx
@@ -10,24 +10,24 @@ const KakaoRedirectHandler = () => {
     const redirect_uri = process.env.REACT_APP_KAKAO_REDIRECT_URI
 
     const getKakaoToken = () => {
-        axios({
-            url: '/api/user/login',
-            method: 'POST',
-            headers: {'Content-Type':'application/json'},
-            data: {'code' : KAKAO_CODE}
-        })
-        .then(res => {
-            if(res.data.loginResult !== '' && res.data.loginResult.substr(0, 7) === 'eyJ0eXA'){
-                localStorage.setItem('token', res.data.loginResult)
-                window.location.href = '/'
-            }
-            else if (res.data.loginResult !== ''){
-                window.location.href = '/signup?userEmail=' + res.data.loginResult
-            }
-    })
-        .catch(err => 
-            console.log(err)
-        )
+    //     axios({
+    //         url: '/api/user/login',
+    //         method: 'POST',
+    //         headers: {'Content-Type':'application/json'},
+    //         data: {'code' : KAKAO_CODE}
+    //     })
+    //     .then(res => {
+    //         if(res.data.loginResult !== '' && res.data.loginResult.substr(0, 7) === 'eyJ0eXA'){
+    //             localStorage.setItem('token', res.data.loginResult)
+    //             window.location.href = '/'
+    //         }
+    //         else if (res.data.loginResult !== ''){
+    //             window.location.href = '/signup?userEmail=' + res.data.loginResult
+    //         }
+    // })
+    //     .catch(err => 
+    //         console.log(err)
+    //     )
     }
 
 
