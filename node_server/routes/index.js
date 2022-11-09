83fa73ede4f703aeae6e366848636e8a05b68ea0
한제규 <anfidthtn@naver.com>
2022-11-09 10:25:03 +0900

    [Infra-JG-4-2] fix : nodeexpress 호출 주소 수정

--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const genFont = require('../generateTTF');
 
-router.post('/makefont', (req, res) => {
+router.post('/nodeexpress/makefont', (req, res) => {
     console.log(req.body)
     genFont(req.body.fontName);
     let seq = req.body.fontseq;
