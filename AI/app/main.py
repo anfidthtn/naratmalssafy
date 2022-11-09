a0f602337a6f6570604b6f2deb751cd2ea699b1e
한제규 <anfidthtn@naver.com>
2022-11-09 23:05:15 +0900

    [Infra-JG-4-1] feat : 폰트 제작 완료 시 file정보 테이블 등록

--- AI/app/main.py
+++ AI/app/main.py
@@ -2,8 +2,6 @@ from typing import Union
 
 from fastapi import FastAPI
 
-import json
-
 import os
 
 import sys
@@ -14,6 +12,13 @@ from fontmaker.generate import FontMaker
 
 from pydantic import BaseModel
 
+from s3linker import s3_connection
+
+import models_, database
+
+
+
+
 
 app = FastAPI()
 
@@ -23,12 +28,26 @@ class Item(BaseModel):
 
 pwd = os.path.dirname(os.path.realpath(__file__))
 
+s3 = s3_connection()
+
 
 @app.post("/fastapi/makefont/")
 def read_root(data : Item):
-    print(data)
-    maker = FontMaker(data.fontName)
-    print(maker)
-    maker.makeTTF(data.fontSeq, data.fontName)
+    savedURL = 'https://naratmalssafy.s3.ap-northeast-2.amazonaws.com/fontname.ttf'
+    try:
+        s3.upload_file(os.path.join("fontmaker", "FONT", data.fontName, "ttf_fonts", data.fontName + ".ttf"),"naratmalssafy", data.fontName + ".ttf")
+        savedURL = 'https://naratmalssafy.s3.ap-northeast-2.amazonaws.com/' + data.fontName + ".ttf"
+    except Exception as e:
+        print(e)
+
+    session = database.Session(database.engine)
     
-    return {"a" : "a"}
+    db_file = models_.TFile(file_original_name=data.fontName + '.ttf', file_saved_name=data.fontName + '.ttf', file_saved_path=savedURL)
+    
+    session.add(db_file)
+    db_font = session.query(models_.TFont).filter(models_.TFont.font_seq == data.fontSeq).first()
+    db_font.t_file = db_file
+    
+    
+    session.commit()
+    return {"savedURL" : savedURL}
