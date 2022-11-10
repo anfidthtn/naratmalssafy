8de7e9b48a8170909b7f22f8ff52483e385c6ab2
한제규 <anfidthtn@naver.com>
2022-11-10 17:17:17 +0900

    [Infra-JG-4-3] fix : nodeexpress 비동기 처리 순서 정리

--- node_server/routes/index.js
+++ node_server/routes/index.js
@@ -4,8 +4,7 @@ const genFont = require('../generateTTF');
 
 router.post('/nodeexpress/makefont', (req, res) => {
     console.log(req.body)
-    genFont(req.body.fontNameHash, req.body.fontName);
-    res.end()
+    genFont(req.body.fontNameHash, req.body.fontName, res);
 });
 
 module.exports = router;
