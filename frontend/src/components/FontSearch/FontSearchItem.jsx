5aa9d8d6e270cba4b123988fd06b75dc0fd8b4f7
jimdac3464 <jimdac3464@gmail.com>
2022-11-10 17:30:37 +0900

    [FE-yjh-8] 백엔드 API 연결 30%

--- frontend/src/components/FontSearch/FontSearchItem.jsx
+++ frontend/src/components/FontSearch/FontSearchItem.jsx
@@ -21,23 +21,23 @@ const FontSearchItem = ({ idx, fontData, fontEditorText }) => {
 
     // 서버에서 웹폰트 넘겨줄 경우 폰트 다운로드 후 적용
 
-    // let font = new FontFace(
-    //   "Gamja Flower",
-    //   `url(https://fonts.gstatic.com/s/gamjaflower/v20/6NUR8FiKJg-Pa0rM6uN40Z4kzJdTdNPFFRJ7lwb-CZch2ydaLb0K.0.woff2) format("woff2")`
-    // );
-    // font
-    //   .load()
-    //   .then(function (loadedFont) {
-    //     document.fonts.add(loadedFont);
-    //     //do something after the font is loaded
-    //     console.log(loadedFont);
-    //   })
-    //   .catch(function (error) {
-    //     // error occurred
-    //   });
+    let font = new FontFace(
+      "지홍체",
+      `url(https://naratmalssafy.s3.ap-northeast-2.amazonaws.com/fo222.woff) format("woff")`
+    );
+    font
+      .load()
+      .then(function (loadedFont) {
+        document.fonts.add(loadedFont);
+        //do something after the font is loaded
+        // console.log(loadedFont);
+      })
+      .catch(function (error) {
+        // error occurred
+      });
 
     fontDataDiv.style.fontFamily = fontData.fontFamilyName;
-    // fontDataDiv.style.fontFamily = "Gamja Flower";
+    // fontDataDiv.style.fontFamily = "지홍체";
     fontDataTextArea.style.fontFamily = fontData.fontFamilyName;
   }, []);
 
