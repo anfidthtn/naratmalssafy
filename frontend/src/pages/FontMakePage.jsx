bbe065bf7941488bbeb1d74a70c203041d777ac7
jimdac3464 <jimdac3464@gmail.com>
2022-11-14 15:39:06 +0900

    [FE-yjh-9] 추가 오류 및 디자인 수정

--- frontend/src/pages/FontMakePage.jsx
+++ frontend/src/pages/FontMakePage.jsx
@@ -573,13 +573,14 @@ const FontMakePage = () => {
               </div>
               <div className="complted_info_text_box">
                 <div className="completed_info_text">
-                  *제작 완료까지 <span style={{ color: "red" }}>20분</span>정도
-                  소요 될 수 있습니다.
+                  *제작 완료까지 최소<span style={{ color: "red" }}>20분</span>
+                  소요 됩니다 단, 제작 요청이 많을 경우 추가적인 시간이 더 소요
+                  됩니다.
                 </div>
                 <div className="completed_info_text">
-                  *제작이 완료된 후에{" "}
-                  <span style={{ color: "red" }}>카카오톡 </span>알림으로 안내해
-                  드립니다.
+                  *제작이 완료된 후에 카카오톡에 등록된{" "}
+                  <span style={{ color: "red" }}>이메일로 </span>
+                  안내해 드립니다.
                 </div>
                 <div className="completed_info_text">
                   *제작이 완료된 후에{" "}
